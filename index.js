b4f95c37dd6b2a87c8e71e2ea46d6c89743972dc
CojocaruCristian <51245957+cojocaruCristian@users.noreply.github.com>
2019-11-11 20:45:35 +0200

    lesson-19-20

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ let numberText = document.getElementById("h4")
 
 firstB.onclick = function(){
     numberText.innerHTML = "You clicked First button";
+    console.log("capturing", this.tagName)
 }
 secondB.onclick = function(){
     numberText.innerHTML = "You clicked Second button"
+    console.log("capturing", this.tagName)
+
 }
 
 thirdB.onclick = function(){
     numberText.innerHTML = "You clicked Third button"
+    console.log("capturing", this.tagName)
+
 }
 
 
