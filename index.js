aa125cd278a0eba0530516bbb67ec394273fce1f
CojocaruCristian <51245957+cojocaruCristian@users.noreply.github.com>
2019-11-11 19:29:36 +0200

    lesson-19

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let numberText = document.getElementById("h4")
+
+
+
+
+firstB.onclick = function(){
+    numberText.innerHTML = "You clicked First button";
+}
+secondB.onclick = function(){
+    numberText.innerHTML = "You clicked Second button"
+}
+
+thirdB.onclick = function(){
+    numberText.innerHTML = "You clicked Third button"
+}
+
+
+
+
+
